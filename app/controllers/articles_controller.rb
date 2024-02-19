76a76b115dca1570d99e4a7adfab3a043ba402c0
Gor <7495500@gmail.com>
2024-02-19 21:26:38 +0300

    Add basic app configuration

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show edit update destroy ]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit; end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save ? (redirect_to article_url(@article)) : (render :new)
+  end
+
+  def update
+    @article.update(article_params) ? (redirect_to article_url(@article)) : (render :edit)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_url
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+end
